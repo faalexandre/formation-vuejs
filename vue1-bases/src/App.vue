ee256bb9d5d8689b8be74f6b762702f522b4e35f
Faustino Alexandre <faustinoalfredoa@outlook.com>
2024-11-24 20:23:49 +0100

    les premiers bases de vue

---
+++ vue1-bases/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {ref} from 'vue'
+
+const msg = ref('Mon premier destinataire')
+</script>
+
+<template>
+  <main>
+<h1>Message à: {{ msg.toUpperCase() }}</h1>
+  </main>
+</template>
+
+<style scoped>
+
+
+</style>
