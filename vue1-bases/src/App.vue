a8c96c7606ef0883bac3f929ed89675c261ad297
Faustino Alexandre <faustinoalfredoa@outlook.com>
2024-11-24 20:41:42 +0100

    2.Détecter un clic avec @click et déclencher une fonction

--- vue1-bases/src/App.vue
+++ vue1-bases/src/App.vue
@@ -1,16 +1,31 @@
 <script setup>
 import {ref} from 'vue'
 
-const msg = ref('Mon premier destinataire')
+// var
+let msg = ref('luanda','Modifier le nombre')
+const count = ref(0)
+const changeMsg = ()=>{
+  msg.value = 'Faustino'
+}
+
+const resetcount = ()=>{
+  count.value = 0
+}
+
 </script>
 
 <template>
   <main>
-<h1>Message à: {{ msg.toUpperCase() }}</h1>
+<h1 @click="changeMsg">{{ msg }}</h1>
+<h2>Nombre = {{ count }}</h2>
+<button @click="count++">Incrémenter le nombre</button>
+<button @click="resetcount">Réinitialiser le nombre</button>
   </main>
 </template>
 
 <style scoped>
-
+h1:hover {
+  cursor: pointer;
+}
 
 </style>
